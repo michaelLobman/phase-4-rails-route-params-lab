8c7eb783bfe15ec7a87d3ba3750fd8f38297fa1e
Michael Lobman <michael.lobman@gmail.com>
2022-02-08 20:50:45 -0500

    Complete assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      students = Student.where(first_name: params[:name].titleize) 
+        .or(Student.where(last_name: params[:name].titleize))
+      render json: students
+    else
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
